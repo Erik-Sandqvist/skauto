297e23bfc94f732f0faca30ad5756cf1a552de53
0412saer <0412saer@JKP-UJCLT813>
2023-03-23 14:22:11 +0100

    Add project files.

---
+++ skauto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace skauto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
