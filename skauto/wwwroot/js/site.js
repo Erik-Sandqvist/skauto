e18d0a0a772ed302305257441a814e631f1ad589
0412saer <0412saer@JKP-UJCLT813>
2023-05-11 10:44:13 +0200

    11-05

--- skauto/wwwroot/js/site.js
+++ skauto/wwwroot/js/site.js
@@ -1,3 +1,22 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+var num1 = 5;
+var num2 = 3;
+var result = num1 * num2;
+
+console.log(result);
+
+var form = document.getElementById('multiplicationForm');
+var resultElement = document.getElementById('result');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Förhindra att formuläret skickas
+
+    var num1 = parseFloat(document.getElementById('num1Input').value);
+    var num2 = parseFloat(document.getElementById('num2Input').value);
+
+    var result = num1 * num2;
+
+    resultElement.textContent = 'Resultat: ' + result;
+});
